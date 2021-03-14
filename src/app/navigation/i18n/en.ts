3225b18135e8b1b7358b53b9029f3a3c65284319
Ameni AKKARI <akkeriamani@gmail.com>
2021-03-14 09:13:21 +0100

    sprint 1 - auth

--- src/app/navigation/i18n/en.ts
+++ src/app/navigation/i18n/en.ts
@@ -7,45 +7,10 @@ export const locale = {
                 TITLE: "Home",
                 BADGE: "25",
             },
-            PROFILES: {
-                TITLE: "Profiles",
-            },
-            DEPARTMENT: {
-                TITLE: "Departments",
-                BADGE: "28",
-            },
-            BUSINESSGROUPS: {
-                TITLE: "Business Groups",
-            },
-            MASTER: {
-                TITLE: "Territorial organization",
-            },
-            ADMIN: {
-                TITLE: "Administration",
-            },
-            USERS: {
-                TITLE: "Users",
-            },
-
-            OPENAPI: {
-                TITLE: "OpenAPI",
-            },
-            MARKETPLACE: {
-                TITLE: "Marketplace",
-                BADGE: "27",
-            },
-            ENVIRONNEMENT: {
-                TITLE: "Environment",
-            },
-            SERVER: {
-                TITLE: "Server",
-            },
-            PLATEFORME: {
-                TITLE: "Platform",
-            },
-            ACCESSMATRIX: {
-                TITLE: "Access Matrix",
-            },
+            ACCOUNTS: {
+                TITLE: "Accounts",
+                BADGE: "25"
+            }
         },
     },
 };
